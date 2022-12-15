faebd30d24a584e292116e0f686878ba21b32217
Grégory <greg.marini@hotmail.fr>
2022-12-15 15:45:41 +0100

    add second input for decrypt message

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
-  const input = document.querySelector("input");
-  console.log(input.value);
+  const input = document.querySelector("#message-input");
+  const encrypted = btoa(input.value);
+
+  const linkInput = document.querySelector("#link-input");
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
 });
